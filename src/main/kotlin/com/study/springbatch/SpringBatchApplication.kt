40c8ef0d71774777bdec764a942cf85fb7f8040f
heshAlgo <serverdeveloper@naver.com>
2023-05-08 10:57:09 +0900

    mysql 연동 및 심플 잡 구성

--- src/main/kotlin/com/study/springbatch/SpringBatchApplication.kt
+++ src/main/kotlin/com/study/springbatch/SpringBatchApplication.kt
@@ -1,8 +1,10 @@
 package com.study.springbatch
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+@EnableBatchProcessing
 @SpringBootApplication
 class SpringBatchApplication
 
