7f7d5c5075390603883d99480a26844023b6a76d
heshAlgo <serverdeveloper@naver.com>
2023-06-06 15:58:21 +0900

    패키지 제거

--- src/main/kotlin/com/study/springbatch/SpringBatchApplication.kt
+++ src/main/kotlin/com/study/springbatch/SpringBatchApplication.kt
@@ -4,13 +4,9 @@ import org.springframework.batch.core.Job
 import org.springframework.batch.core.JobParametersBuilder
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.batch.core.launch.JobLauncher
-import org.springframework.boot.ApplicationRunner
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.ConfigurableApplicationContext
-import java.util.*
-import kotlin.system.exitProcess
 
 @EnableBatchProcessing
 @SpringBootApplication
